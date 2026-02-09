afb00d1f976f7a19a35fe1b4fb7e3b7f77655c33
Renzis <cpkddp@gmail.com>
2026-02-09 17:07:31 -0300

    Remove arquivos .p

--- 10Salary with bonus.py
+++
@@ -1,9 +0,0 @@
-SellerName = input("")
-FixedSalary = float(input())
-SalesTotal = float(input())
-Bonus = 0.15
-FinalSalary = 0
-
-FinalSalary = FixedSalary + (SalesTotal * Bonus)
-
-print("TOTAL = R$ %.2f"% FinalSalary)
--- 9Salary.py
+++
@@ -1,8 +0,0 @@
-EmployeeNumber = int(input())
-WorkedHours = float(input())
-RecievedHour = float(input())
-
-EndMonth = WorkedHours * RecievedHour
-
-print("NUMBER =", EmployeeNumber)
-print("SALARY = U$ %.2f"% EndMonth)
